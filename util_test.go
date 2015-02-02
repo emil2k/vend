524290a33e6da5655ca69be1d72bb5dcc86cb7d8
Emil Davtyan <emil2k@gmail.com>
2015-02-02 12:40:15 -0600

    Moved test utilities.

--- util_test.go
+++ util_test.go
@@ -1,17 +1,58 @@
 package main
 
 import (
+	"go/build"
+	"io/ioutil"
+	"os"
+	"reflect"
+	"sort"
 	"testing"
 )
 
-// TestHasString tests the utility function hasString, checking both when a
-// string is present and not present.
-func TestHasString(t *testing.T) {
-	test := []string{"a", "b", "c"}
-	if x := hasString(test, "a"); !x {
-		t.Errorf("expected to have")
+// testBuild tests that the package in the passed directory builds without
+// errors and returns the package.
+func testBuild(t *testing.T, dir string) *build.Package {
+	pkg, err := build.ImportDir(dir, 0)
+	if err != nil {
+		t.Errorf("error during build : %s", err.Error())
 	}
-	if x := hasString(test, "d"); x {
-		t.Errorf("expeted no to have")
+	return pkg
+}
+
+// testImports tests that the package in the passed directory has the expected
+// imports. Includes imports from test files and external test files based on
+// `includeTests` bool.
+func testImports(t *testing.T, dir string, imports []string, includeTests bool) {
+	pkg := testBuild(t, dir)
+	gotImports := getImports(pkg, includeTests) // sorts it already
+	sort.Strings(imports)
+	if !reflect.DeepEqual(gotImports, imports) {
+		t.Errorf("imports not equal : got %v, expected %v", gotImports, imports)
 	}
+}
+
+// getTestContextCopy creates a temporary directory, copies the contents from
+// src into it, and returns a context with it as set as the GOPATH.
+// In case of error immediately failst the test.
+func getTestContextCopy(t *testing.T, src string) *build.Context {
+	ctx := getTestContext(t)
+	if err := copyDir(src, ctx.GOPATH); err != nil {
+		t.Errorf("error while copying GOPATH : %s", err.Error())
+		t.FailNow()
+	}
+	return ctx
+}
+
+// getTestContext creates a temporary directory and returns a context with it as
+// set as the GOPATH.
+// In case of error immediately fails the test.
+func getTestContext(t *testing.T) *build.Context {
+	dst, err := ioutil.TempDir(os.TempDir(), "vendtest")
+	if err != nil {
+		t.Errorf("error getting temporary directory : %s", err.Error())
+		t.FailNow()
+	}
+	ctx := build.Default
+	ctx.GOPATH = dst
+	return &ctx
 }
