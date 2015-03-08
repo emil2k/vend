083fdf69cfa6af6a99b23ab707a21ba6569fa3d1
Emil Davtyan <emil2k@gmail.com>
2015-03-07 22:45:09 -0600

    List usages of dependencies.
    Prints out the packages that are using the given import, helpful when
    using list with the recurse flag.
    
    Closes #8.

--- info.go
+++ info.go
@@ -11,7 +11,8 @@ import (
 // at the specified path, relative paths are resolved from the current working
 // directory.
 func list(ctx *build.Context, cwd, path string) error {
-	imps := make([]string, 0)
+	imps := make(map[string][]*build.Package, 0)
+	impKeys := make([]string, 0) // for sorting later
 	var parentPkg *build.Package
 	process := func(pkg *build.Package, err error) error {
 		// Set the parent package so child filters work properly as the
@@ -21,7 +22,13 @@ func list(ctx *build.Context, cwd, path string) error {
 		}
 		f := listFilter(ctx, cwd, parentPkg.ImportPath, opt.child, opt.standard)
 		for _, add := range filterImports(getImports(pkg, opt.tests), f) {
-			imps = appendUnique(imps, add)
+			impKeys = appendUnique(impKeys, add)
+			// Keep track of packages that use each import.
+			if mentions, ok := imps[add]; ok {
+				imps[add] = append(mentions, pkg)
+			} else {
+				imps[add] = []*build.Package{pkg}
+			}
 		}
 		return nil
 	}
@@ -38,12 +45,19 @@ func list(ctx *build.Context, cwd, path string) error {
 		process(pkg, nil)
 	}
 	// Output the imports
-	sort.Strings(imps)
-	for _, imp := range imps {
+	sort.Strings(impKeys)
+	for _, imp := range impKeys {
 		if opt.quite {
 			fmt.Println(imp)
 		} else if err := info(ctx, cwd, imp); err != nil {
 			return err
+		} else if opt.verbose {
+			// Output packages that use the import.
+			fmt.Println("\nUsages :")
+			for _, mention := range imps[imp] {
+				fmt.Printf("%s (%s)\n", mention.ImportPath, mention.Name)
+			}
+			fmt.Println()
 		}
 	}
 	return nil
@@ -88,10 +102,12 @@ func info(ctx *build.Context, cwd, path string) error {
 	}
 	// Verbose output
 	if opt.verbose {
-		fmt.Println("  Standard :\t", pkg.Goroot)
-		fmt.Println("  Directory :\t", pkg.Dir)
+		fmt.Printf("\nStandard :\n%t\n", pkg.Goroot)
+		if len(pkg.Dir) > 0 {
+			fmt.Printf("Directory :\n%s\n", pkg.Dir)
+		}
 		if len(pkg.AllTags) > 0 {
-			fmt.Println("  Tags :\t",
+			fmt.Printf("Tags :\n%s\n",
 				strings.Join(pkg.AllTags, " "))
 		}
 	}
